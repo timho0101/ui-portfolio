bcc85b71943fcad81e09ab080d8e7d371a73f6f2
Ho <hoti@repsrv.com>
2020-01-22 16:34:28 -0700

    fix routing issues

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,41 +2,14 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { AboutModule } from '../elements/about/about.module';
-import { ContactModule } from '../elements/contact/contact.module';
-import { LazyLoadingModule } from '../elements/lazy-loading/lazy-loading.module';
-import { RsCarouselControlModule } from '../elements/rs-carousel-control/rs-carousel-control.module';
-import { AngularPipesModule } from '../elements/angular-pipes/angular-pipes.module';
-import { ToDoListModule } from '../elements/to-do-list/to-do-list.module';
-import { NpmCommandModule } from '../elements/npm-command/npm-command.module';
-import { NotesModule } from '../elements/notes/notes.module';
-import { ApiServerModule } from '../elements/api-server/api-server.module';
-import { LocalSessionStorageModule } from '../elements/local-session-storage/local-session-storage.module';
-import { RsComModule } from '../elements/rs-com-notes/rs-com-notes.module';
 import { CssGridLayoutModule } from '../elements/css-grid-layout/css-grid-layout.module';
 import { ElementsModule } from '../elements/elements.module';
-import { MatSidenavModule, MatToolbarModule, MatMenuModule, MatIconModule, MatGridListModule, MatButtonModule } from '@angular/material';
-import { NavComponent } from './nav/nav.component';
+import { MatMenuModule } from '@angular/material';
 import { LeftSideNavBarComponent } from './left-side-nav-bar/left-side-nav-bar.component';
-import { AngularComponent } from './left-side-nav-bar/angular/angular.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 
 const routes: Routes = [
   {
     path: '', component: MainComponent, children: [
-      {path: 'about', loadChildren: () => AboutModule},
-      {path: 'contact', loadChildren: () => ContactModule},
-      {path: 'lazyLoading', loadChildren: () => LazyLoadingModule},
-      {path: 'rsCarouselControl', loadChildren: () => RsCarouselControlModule},
-      {path: 'pipes', loadChildren: () => AngularPipesModule},
-      {path: 'todo', loadChildren: () => ToDoListModule},
-      {path: 'npmCommand', loadChildren: () => NpmCommandModule},
-      {path: 'notes', loadChildren: () => NotesModule},
-      {path: 'apiServer', loadChildren: () => ApiServerModule},
-      {path: 'localSessionStorage', loadChildren: () => LocalSessionStorageModule},
-      {path: 'rsComNotes', loadChildren: () => RsComModule},
-      {path: 'cssGridLayout', loadChildren: () => CssGridLayoutModule},
       {path: '', loadChildren: () => ElementsModule}
     ]
   },
@@ -46,22 +19,13 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatToolbarModule,
     MatMenuModule,
-    MatIconModule,
-    MatGridListModule,
     RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
   declarations: [
     MainComponent,
-    NavComponent,
-    LeftSideNavBarComponent,
-    AngularComponent,
-    FooterComponent,
-    BodyComponent
+    LeftSideNavBarComponent
   ],
   providers: []
 })
--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,16 +2,66 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookOfFlamesModule } from './book-of-flames/book-of-flames.module';
 import { BookOfWatersModule } from './book-of-waters/book-of-waters.module';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LazyLoadingComponent } from './lazy-loading/lazy-loading.component';
+import { AngularPipesComponent } from './angular-pipes/angular-pipes.component';
+import { ToDoListModule } from './to-do-list/to-do-list.module';
+import { NpmCommandComponent } from './npm-command/npm-command.component';
+import { NotesComponent } from './notes/notes.component';
+import { ApiServerComponent } from './api-server/api-server.component';
+import { LocalSessionStorageComponent } from './local-session-storage/local-session-storage.component';
+import { CssGridLayoutModule } from './css-grid-layout/css-grid-layout.module';
+import {
+  MatSidenavModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatGridListModule,
+  MatCardModule
+} from '@angular/material';
+import { PipesModule } from '../shared/pipes/pipes.module';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   {path: 'bookOfFlame', loadChildren: () => BookOfFlamesModule},
   {path: 'bookOfWater', loadChildren: () => BookOfWatersModule},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'lazyLoading', component: LazyLoadingComponent},
+  {path: 'pipes', component: AngularPipesComponent},
+  {path: 'todo', loadChildren: () => ToDoListModule},
+  {path: 'npmCommand', component: NpmCommandComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'apiServer', component: ApiServerComponent},
+  {path: 'localSessionStorage', component: LocalSessionStorageComponent},
+  {path: 'cssGridLayout', loadChildren: () => CssGridLayoutModule}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    PipesModule,
+    CommonModule,
+    RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [],
+  declarations: [
+    AboutComponent,
+    ContactComponent,
+    LazyLoadingComponent,
+    AngularPipesComponent,
+    NpmCommandComponent,
+    NotesComponent,
+    ApiServerComponent,
+    LocalSessionStorageComponent
+  ],
   providers: []
 })
 export class ElementsModule {}
