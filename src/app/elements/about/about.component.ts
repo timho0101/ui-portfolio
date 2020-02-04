1b89891a063015f4a23e41470de19901d20902a8
Ho <hoti@repsrv.com>
2020-02-04 14:22:50 -0700

    take out Home tag

--- src/app/elements/about/about.component.ts
+++ src/app/elements/about/about.component.ts
@@ -7,10 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  public about = `
-    just storing everything i know about angular
-  `;
-
   ngOnInit() {
   }
 
--- src/app/elements/contact/contact.component.ts
+++ src/app/elements/contact/contact.component.ts
@@ -6,15 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  public email = `
-    Email: github.timho@gmail.com
-  `;
-
-  public gitHub = `
-    GitHub: github.com/Voslan
-  `;
-
   ngOnInit() {
   }
 
