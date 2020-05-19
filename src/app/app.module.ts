019efee3e2ec4e5aacb92558faf4b5b98251f97c
Tim Ho <github.timho@gmail.com>
2020-05-19 15:41:40 -0700

    refactor router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { ElementsModule } from './elements/elements.module';
 import { SharedModule } from './shared/shared.module';
 import { CommonModule } from '@angular/common';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', loadChildren: () => CoreModule}
+];
 
 @NgModule({
   declarations: [
@@ -16,12 +21,12 @@ import { CommonModule } from '@angular/common';
     CoreModule,
     ElementsModule,
     SharedModule,
-    AppRoutingModule,
     BrowserModule,
     CommonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
