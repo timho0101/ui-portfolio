af2831c7cb644c999b79a448e35eafca9ffe9ce7
Tim Ho <github.timho@gmail.com>
2020-05-19 17:10:13 -0700

    refractor: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent {
-  title = 'Game Trader';
-}
+export class AppComponent {}
--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -3,7 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styles: [`
+  .verticalLine {
+    border-right: 1px dashed lightslategrey;
+  }
+  
+  html,
+  body {
+   height: 100%;
+   position: relative;
+  }
+  .container {
+   min-height: 100vh; /* will cover the 100% of viewport */
+   overflow: hidden;
+   display: block;
+   position: relative;
+   padding-bottom: 100px; /* height of your footer */
+  }
+  .footer {
+   position: absolute;
+   bottom: 0;
+   width: 100%;
+  }
+  
+  .nav-bar {
+    margin-top: 15px;
+  }
+  
+  a {
+    margin-right: 15px;
+    text-decoration: none;
+    font-size: 16px;
+    color: lightslategrey;
+    display: inline;
+  }
+  
+  
+  .float-right {
+    float: right;
+  }
+  `]
 })
 export class MainComponent implements OnInit {
 
