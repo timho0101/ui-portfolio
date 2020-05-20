6910a1480e933c61663602b4fafee0c1f7ce83a7
Tim Ho <github.timho@gmail.com>
2020-05-20 12:49:50 -0700

    delete unused code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { CoreModule } from './core/core.module';
+
+// const routes: Routes = [
+//   {path: '', loadChildren: () => CoreModule}
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
