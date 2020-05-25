ec07fcf3c4a863687b51b727bbd44005eb9090ab
Tim Ho <github.timho@gmail.com>
2020-05-24 22:19:08 -0700

    delete npmCommand component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,7 +5,6 @@ import { ContactComponent } from './contact/contact.component';
 import { LazyLoadingComponent } from './lazy-loading/lazy-loading.component';
 import { AngularPipesComponent } from './angular-pipes/angular-pipes.component';
 import { ToDoListModule } from './to-do-list/to-do-list.module';
-import { NpmCommandComponent } from './npm-command/npm-command.component';
 import { NotesComponent } from './notes/notes.component';
 import { ApiServerComponent } from './api-server/api-server.component';
 import { LocalSessionStorageComponent } from './local-session-storage/local-session-storage.component';
@@ -37,7 +36,6 @@ const routes: Routes = [
   {path: 'lazyLoading', component: LazyLoadingComponent},
   {path: 'pipes', component: AngularPipesComponent},
   {path: 'todo', loadChildren: () => ToDoListModule},
-  {path: 'npmCommand', component: NpmCommandComponent},
   {path: 'notes', component: NotesComponent},
   {path: 'apiServer', component: ApiServerComponent},
   {path: 'localSessionStorage', component: LocalSessionStorageComponent},
@@ -65,7 +63,6 @@ const routes: Routes = [
     ContactComponent,
     LazyLoadingComponent,
     AngularPipesComponent,
-    NpmCommandComponent,
     NotesComponent,
     ApiServerComponent,
     LocalSessionStorageComponent,
