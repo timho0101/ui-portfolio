564c0c2f729a2c649e39a499b08e20d7d44b86b6
Tim Ho <github.timho@gmail.com>
2020-05-31 11:30:21 -0700

    Rename: side-nav class

--- src/app/core/side-nav/side-nav.component.ts
+++ src/app/core/side-nav/side-nav.component.ts
@@ -3,16 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-nav',
   template: `
-    <div class="css-v0mdt">
-      <ul class="leafygreen-ui-bdnco">
+    <div class="container">
+      <ul class="wrapper">
         <li>
-          <h4 class="leafygreen" >Projects</h4>
-          <ul role="menu" class="leafygreen-ui-bdnco">
-            <li role="none"><a class="leafygreen-ui-1383ole" [routerLink]="['/natour']"><div class="leafygreen-ui-1aen1ds">Natour</div></a></li>
-            <li role="none"><a class="leafygreen-ui-1383ole"><div class="leafygreen-ui-1aen1ds">App 2</div></a></li>
-            <li role="none"><a class="leafygreen-ui-1383ole"><div class="leafygreen-ui-1aen1ds">App 3</div></a></li>
-            <li role="none"><a class="leafygreen-ui-1383ole"><div class="leafygreen-ui-1aen1ds">App 4</div></a></li>
-            <li role="none"><a class="leafygreen-ui-1383ole"><div class="leafygreen-ui-1aen1ds">App 5</div></a></li>
+          <h4 class="ui-title">Projects</h4>
+          <ul role="menu" class="wrapper">
+            <li role="none"><a class="li-leafygreen" [routerLink]="['/natour']"><div class="leafygreen-ui-1aen1ds">Natour</div></a></li>
+            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 2</div></a></li>
+            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 3</div></a></li>
+            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 4</div></a></li>
+            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 5</div></a></li>
           </ul>
         </li>
       </ul>
