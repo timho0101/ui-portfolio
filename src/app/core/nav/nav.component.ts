948f760f59849073704725e8aaf9047ae75a7470
Tim Ho <github.timho@gmail.com>
2020-05-31 18:53:24 -0700

    add: html file for nav & side-nav

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -2,16 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
-  template: `
-    <div class="div-container wrapper">
-      <div class="nav-float-left">
-      <a href="#">Home</a>
-      <a>About</a>
-      <a>Contact</a>
-      <div></div>
-      </div>
-    </div>
-  `,
+  templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
--- src/app/core/side-nav/side-nav.component.ts
+++ src/app/core/side-nav/side-nav.component.ts
@@ -2,22 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav',
-  template: `
-    <div class="container">
-      <ul class="wrapper">
-        <li>
-          <h4 class="ui-title">Projects</h4>
-          <ul role="menu" class="wrapper">
-            <li role="none"><a class="li-leafygreen" [routerLink]="['/natour']"><div class="leafygreen-ui-1aen1ds">Natour</div></a></li>
-            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 2</div></a></li>
-            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 3</div></a></li>
-            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 4</div></a></li>
-            <li role="none"><a class="li-leafygreen"><div class="leafygreen-ui-1aen1ds">App 5</div></a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  `,
+  templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
 
